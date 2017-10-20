d833c8e06e0f2a1378180fe0eb5f0575fdc04d65
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-20 17:20:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,5 @@ function updatePlaylist(playlist, artist, song) {
   return Object.assign({}, playlist, {[artist]: song})
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
-  return playlist
+  return delete playlist.artistName;
 }
